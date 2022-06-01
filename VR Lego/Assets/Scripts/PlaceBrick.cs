46958b2b4fdc445de9d03b497f16a0e1ea4f5d7e
Douwe Raat <douwe.raat@coderclass.nl>
2022-06-01 08:30:44 +0200

    4 dingen toegevoegd
    laser pointer
    delete function
    verschil tussen onder- en bovenkant
    teleporten op geplaatste lego

--- VR Lego/Assets/Scripts/PlaceBrick.cs
+++ VR Lego/Assets/Scripts/PlaceBrick.cs
@@ -9,10 +9,8 @@ public class PlaceBrick : MonoBehaviour
     public Material TransparentMat;
     public Material BrickMat;
     public Material[] materials;
-    public Camera VRCamera;
 
     protected Brick CurrentBrick;
-    protected bool PositionOk = true;
     private int color = 1;
     private int brick = 0;
     public Transform controller;
@@ -25,123 +23,65 @@ public class PlaceBrick : MonoBehaviour
 
     void Start()
     {
-        SetNextBrick();
-}
+        //SetNextBrick();
+    }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.G) || switchColor.GetStateDown(SteamVR_Input_Sources.Any))
-        {
-            color += 2;
-            if (color > (materials.Length-1))
-{
-                color = 1;
-            }
-            if (CurrentBrick != null)
-                CurrentBrick.GetComponent<Renderer>().material = materials[color + 1];
-        }
         if (Input.GetKeyDown(KeyCode.H) || switchBrick.GetStateDown(SteamVR_Input_Sources.Any))
         {
-            brick += 1;
-            if (brick > (PrefabBrick.Length-1))
-            {
-                brick = 0;
-            }
-            if (CurrentBrick != null)
-            {
-                CurrentBrick.GetComponent<Renderer>().material = materials[0];
-
-                var rot = CurrentBrick.transform.rotation;
-                var pos = CurrentBrick.transform.position;
-                GameObject.DestroyImmediate(CurrentBrick.gameObject);
-                CurrentBrick = null;
-                SetNextBrick();
-                CurrentBrick.transform.rotation = rot;
-                CurrentBrick.transform.position = pos;
-            }
+            ChangeBrick();
         }
 
         if (CurrentBrick != null)
         {
-            //snap to grid
-            //if (Physics.Raycast(controller.transform.position, controller.transform.forward, out var hitinfo, float.MaxValue, LegoLogic.LayerMaksLego))
-            if (Physics.Raycast(VRCamera.transform.position, controller.transform.position, out var hitinfo, float.MaxValue, LegoLogic.LayerMaksLego))
+            if (Physics.Raycast(controller.transform.position, controller.transform.forward, out var hitinfo, float.MaxValue, LegoLogic.LayerMaksLego))
             {
+                Gizmos.color = Color.red;
+                Gizmos.DrawLine(controller.transform.position, hitinfo.point);
                 var position = LegoLogic.SnapToGrid(hitinfo.point);
-                //var position = LegoLogic.SnapToGrid(CurrentBrick.transform.position);
-                //var position = (LegoLogic.SnapToGrid(controller.transform.position));
-                var placeposition = position;
-                PositionOk = false;
 
-                if (Input.GetKeyDown(KeyCode.I))
-                {
-                    placeposition.x += LegoLogic.Grid.x;
-                }
-                if (Input.GetKeyDown(KeyCode.K))
-                {
-                    placeposition.x -= LegoLogic.Grid.x;
-                }
-                if (Input.GetKeyDown(KeyCode.L))
-                {
-                    placeposition.z += LegoLogic.Grid.z;
-                }
-                if (Input.GetKeyDown(KeyCode.J))
-                {
-                    placeposition.z -= LegoLogic.Grid.z;
-                }
-                if (Input.GetKeyDown(KeyCode.N))
-                {
-                    placeposition.y -= LegoLogic.Grid.y;
-                }
-
-                //for (int i = 0; i < 100; i++)
+                //if (hitinfo.point.y >= hitinfo.collider.transform.position.y + hitinfo.collider.gameObject.GetComponent<BoxCollider>().size.y * hitinfo.collider.gameObject.transform.localScale.y / 2)
                 //{
-                //    var collider = Physics.OverlapBox(placeposition + CurrentBrick.transform.rotation * CurrentBrick.Collider.center, CurrentBrick.Collider.size / 2, CurrentBrick.transform.rotation, LegoLogic.LayerMaksLego);
-                //    PositionOk = collider.Length == 0;
-                //    if (!PositionOk)
-                //        break;
-                //    else
-                //        placeposition.y -= LegoLogic.Grid.y;
+                //    //raakt bovenkant
                 //}
-                //for (int i = 0; i < 100; i++)
+                //else if (hitinfo.point.y <= hitinfo.collider.transform.position.y - hitinfo.collider.gameObject.GetComponent<BoxCollider>().size.y * hitinfo.collider.gameObject.transform.localScale.y / 2)
                 //{
-                //    var collider = Physics.OverlapBox(placeposition + CurrentBrick.transform.rotation * CurrentBrick.Collider.center, CurrentBrick.Collider.size / 2, CurrentBrick.transform.rotation, LegoLogic.LayerMaksLego);
-                //    PositionOk = collider.Length == 0;
-                //    if (PositionOk)
-                //        break;
-                //    else
+                //    //raakt onderkant
+                //    for (int i = 0; i < Mathf.Round(CurrentBrick.GetComponent<BoxCollider>().size.y / 8); i++)
                 //    {
-                //        placeposition.y += LegoLogic.Grid.y;
+                //        position = new Vector3(position.x, position.y-LegoLogic.Grid.y, position.z);
                 //    }
                 //}
-                if (PositionOk)
-                    CurrentBrick.transform.position = placeposition;
-                else
-                    CurrentBrick.transform.position = position;
-            }
-            //if (Physics.Raycast(VRCamera.transform.position, VRCamera.transform.forward, out var hitinfo, float.MaxValue, LegoLogic.LayerMaksLego))
-            //{
-            //    //snap to grid
-            //    var position = LegoLogic.SnapToGrid(hitinfo.point);
+                //else
+                //{
+                //    //raakt zijkant
+                //    position = LegoLogic.SnapToGrid(CurrentBrick.transform.position);
+                //}
 
-            //    //try to find a collision free position
-            //    var placeposition = position;
-            //    PositionOk = false;
-            //    for (int i = 0; i < 10; i++)
-            //    {
-            //        var collider = Physics.OverlapBox(placeposition + CurrentBrick.transform.rotation * CurrentBrick.Collider.center, CurrentBrick.Collider.size / 2, CurrentBrick.transform.rotation, LegoLogic.LayerMaksLego);
-            //        PositionOk = collider.Length == 0;
-            //        if (PositionOk)
-            //            break;
-            //        else
-            //            placeposition.y += LegoLogic.Grid.y;
-            //    }
-            //    if (PositionOk)
-            //        CurrentBrick.transform.position = placeposition;
-            //    else
-            //        CurrentBrick.transform.position = position;
-            //}
-            if ((Input.GetKeyDown(KeyCode.Space) || placeBrick.GetStateDown(SteamVR_Input_Sources.Any)) && CurrentBrick != null && PositionOk)
+                //if (Input.GetKeyDown(KeyCode.I))
+                //{
+                //    position.x += LegoLogic.Grid.x;
+                //}
+                //if (Input.GetKeyDown(KeyCode.K))
+                //{
+                //    position.x -= LegoLogic.Grid.x;
+                //}
+                //if (Input.GetKeyDown(KeyCode.L))
+                //{
+                //    position.z += LegoLogic.Grid.z;
+                //}
+                //if (Input.GetKeyDown(KeyCode.J))
+                //{
+                //    position.z -= LegoLogic.Grid.z;
+                //}
+                //if (Input.GetKeyDown(KeyCode.N))
+                //{
+                //    position.y -= LegoLogic.Grid.y;
+                //}
+                CurrentBrick.transform.position = position;
+            }
+            if ((Input.GetKeyDown(KeyCode.Space) || placeBrick.GetStateDown(SteamVR_Input_Sources.Any)))
             {
                 Debug.Log("plaatsen werkt");
                 CurrentBrick.Collider.enabled = true;
@@ -155,9 +95,27 @@ public class PlaceBrick : MonoBehaviour
                 CurrentBrick.transform.position = pos;
             }
 
+            if (Input.GetKeyDown(KeyCode.G) || switchColor.GetStateDown(SteamVR_Input_Sources.Any))
+            {
+                ChangeColor();
+            }
+
             if (Input.GetKeyDown(KeyCode.P) || rotateBrick.GetStateDown(SteamVR_Input_Sources.Any))
             {
                 CurrentBrick.transform.Rotate(Vector3.up, 90);
+            }
+        }
+
+        else {
+            if (Physics.Raycast(controller.transform.position, controller.transform.forward, out var hitinfo, float.MaxValue, LegoLogic.LayerMaksLego))
+            {
+                Gizmos.color = Color.red;
+                Gizmos.DrawLine(controller.transform.position, hitinfo.point);
+
+                if ((Input.GetKeyDown(KeyCode.Space) || placeBrick.GetStateDown(SteamVR_Input_Sources.Any)) && hitinfo.collider.gameObject.tag == "LegoDeletable")
+                {
+                    GameObject.DestroyImmediate(hitinfo.collider.gameObject);
+                }
             }
         }
     }
@@ -167,5 +125,46 @@ public class PlaceBrick : MonoBehaviour
         CurrentBrick = Instantiate(PrefabBrick[brick]);
         CurrentBrick.Collider.enabled = false;
         CurrentBrick.GetComponent<Renderer>().material = materials[color+1];
+    }
+
+    private void ChangeColor()
+    {
+        color += 2;
+        if (color > (materials.Length - 1))
+        {
+            color = 1;
+        }
+        if (CurrentBrick != null)
+            CurrentBrick.GetComponent<Renderer>().material = materials[color + 1];
+    }
+
+    private void ChangeBrick()
+    {
+        brick += 1;
+        if (brick > (PrefabBrick.Length - 1))
+        {
+            brick = 0;
+        }
+
+        if (CurrentBrick != null)
+        {
+            CurrentBrick.GetComponent<Renderer>().material = materials[0];
+
+            var rot = CurrentBrick.transform.rotation;
+            var pos = CurrentBrick.transform.position;
+            GameObject.DestroyImmediate(CurrentBrick.gameObject);
+            CurrentBrick = null;
+
+            if (brick != 0)
+            {
+                SetNextBrick();
+                CurrentBrick.transform.rotation = rot;
+                CurrentBrick.transform.position = pos;
+            }
+        }
+        else if (brick != 0)
+        {
+            SetNextBrick();
+        }
     }
 }
